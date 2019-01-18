f0b2aa810b7de1fa8a0190d0fbf6e22dea1dff94
Selije <agaj.wasi@gmail.com>
2019-01-18 18:56:30 +0100

    Associate videos and categories.

--- web/models/video.ex
+++ web/models/video.ex
@@ -5,7 +5,8 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
-    belongs_to :user, Rumbl.User, foreign_key: :user_id
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
     timestamps()
   end
@@ -13,12 +14,14 @@ defmodule Rumbl.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  @required_fields ~w(url title description)
-  @optional_fields ~w()
+  @required_fields ~w(url title description)a  #a makes list of atoms (with ~w only)
+  @optional_fields ~w(category_id)a
 
   def changeset(model, params \\ %{}) do
     model
-    |> cast(params, @required_fields, @optional_fields)
-#    |> validate_required(@required_fields)
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> assoc_constraint(:category)
+
   end
 end
