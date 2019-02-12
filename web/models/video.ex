4c5382b03d63bb8a522d74e693e410f2b8e43370
Selije <agaj.wasi@gmail.com>
2019-02-12 22:12:15 +0100

    Add slugifing URLs.

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,15 +1,16 @@
 defmodule Rumbl.Video do
-  use Rumbl.Web, :model
+    use Rumbl.Web, :model
 
-  schema "videos" do
-    field :url, :string
-    field :title, :string
-    field :description, :string
-    belongs_to :user, Rumbl.User
-    belongs_to :category, Rumbl.Category
+    schema "videos" do
+      field :url, :string
+      field :title, :string
+      field :description, :string
+      belongs_to :user, Rumbl.User
+      belongs_to :category, Rumbl.Category
+      field :slug, :string
 
-    timestamps()
-  end
+      timestamps()
+    end
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
@@ -17,11 +18,27 @@ defmodule Rumbl.Video do
   @required_fields ~w(url title description)a  #a makes list of atoms (with ~w only)
   @optional_fields ~w(category_id)a
 
-  def changeset(model, params \\ %{}) do
-    model
-    |> cast(params, @required_fields ++ @optional_fields)
-    |> validate_required(@required_fields)
-    |> assoc_constraint(:category)
 
-  end
+#cast, assoc_contraint, get_change, put_change are functions defined in Ecto.Changeset
+    def changeset(model, params \\ %{}) do
+        model
+        |> cast(params, @required_fields ++ @optional_fields)
+        |> validate_required(@required_fields)
+        |> slugify_title()
+        |> assoc_constraint(:category)
+    end
+
+    defp slugify_title(changeset) do
+        if title = get_change(changeset, :title) do
+            put_change(changeset, :slug, slugify(title))
+        else
+            changeset
+        end
+    end
+
+    defp slugify(str) do
+        str
+        |> String.downcase()
+        |> String.replace(~r/[^\w-]+/u, "-")
+    end
 end
